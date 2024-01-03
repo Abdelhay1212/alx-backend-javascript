cd06fbaf7e78f49574a2b8106008b2d7cd8b04eb
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-01-03 10:31:06 +0000

    updated

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise.then(() => ({ status: 200, body: 'success' }))
-    .then(() => new Error())
+    .catch(() => new Error())
     .then(() => {
       console.log('Got a response from the API');
     });
