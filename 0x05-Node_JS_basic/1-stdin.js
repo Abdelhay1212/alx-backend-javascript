6fd258089fa406279671b2c29aa99b643da2f528
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-03-04 04:09:46 +0100

    updated

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
 /* eslint-disable linebreak-style */
 const readline = require('readline').createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 });
 
-readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
+readline.question(`Welcome to Holberton School, what is your name?\n`, name => {
   console.log(`Your name is: ${name}`);
   readline.close();
 });
