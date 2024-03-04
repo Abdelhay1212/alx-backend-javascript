194c59142562bd84d91747f830f61247e187b220
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-03-04 03:53:00 +0100

    updated

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,13 @@
 /* eslint-disable linebreak-style */
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (name) => {
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  process.exit();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.log('This important software is now closing');
 });
