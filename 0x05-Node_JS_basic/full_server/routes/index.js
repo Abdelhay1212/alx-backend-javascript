c830055379d5b4431f7e82edfbe9d664107fa0aa
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-03-13 06:39:33 +0100

    updated

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -4,16 +4,8 @@ import StudentsController from '../controllers/StudentsController';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  AppController.getHomepage(req, res);
-});
-
-router.get('/students', (req, res) => {
-  StudentsController.getAllStudents(req, res);
-});
-
-router.get('/students/:major', (req, res) => {
-  StudentsController.getAllStudentsByMajor(req, res);
-});
+router.get('/', AppController.getHomepage);
+router.get('/students', StudentsController.getAllStudents);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
 export default router;
