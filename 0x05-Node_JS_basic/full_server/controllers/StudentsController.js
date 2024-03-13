dc73e62a3c672529461d48d1590e50be6360df76
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-03-13 06:52:18 +0100

    updated

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -6,7 +6,8 @@ class StudentsController {
       .then((data) => {
         let response = 'This is the list of our students\n';
         Object.keys(data).forEach((field) => {
-          response += `Number of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}\n`;
+          response += `Number of students in ${field}: ${data[field].length}. `
+            + `List: ${data[field].join(', ')}\n`;
         });
         res.status(200).send(response.slice(0, response.length - 1));
       }).catch((err) => {
@@ -20,6 +21,9 @@ class StudentsController {
     }
     readDatabase(process.argv[2])
       .then((data) => {
+        if (!data[req.params.major]) {
+          res.status(500).send('Cannot load the database');
+        }
         res.status(200).send(`List: ${data[req.params.major].join(', ')}`);
       }).catch((err) => {
         res.status(500).send(`Cannot load the database - ${err.message}`);
