f43f6b7fd12d99f88ff623f118c98363baa2895d
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-03-15 06:37:51 +0100

    updated"

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,32 +1,35 @@
 import readDatabase from '../utils';
 
 class StudentsController {
-  static getAllStudents(req, res) {
+  static getAllStudents(request, response) {
     readDatabase(process.argv[2])
       .then((data) => {
-        let response = 'This is the list of our students\n';
-        Object.keys(data).forEach((field) => {
-          response += `Number of students in ${field}: ${data[field].length}. `
-            + `List: ${data[field].join(', ')}\n`;
-        });
-        return res.status(200).send(response.slice(0, response.length - 1));
-      }).catch((err) => {
-        res.status(500).send(`Cannot load the database - ${err.message}`);
+        let output = 'This is the list of our students\n';
+        for (const key in data) {
+          if (Object.hasOwnProperty.call(data, key)) {
+            output += `Number of students in ${key}: ${data[key].length}. List: ${data[key].join(', ')}\n`;
+          }
+        }
+        return response.status(200).send(output);
+      })
+      .catch(() => {
+        response.status(500).send('Cannot load the database');
       });
   }
 
-  static getAllStudentsByMajor(req, res) {
-    if (!['CS', 'SWE'].includes(req.params.major)) {
-      return res.status(500).send('Major parameter must be CS or SWE');
+  static getAllStudentsByMajor(request, response) {
+    if (!['CS', 'SWE'].includes(request.params.major)) {
+      return response.status(500).send('Major parameter must be CS or SWE');
     }
     return readDatabase(process.argv[2])
       .then((data) => {
-        if (!data[req.params.major]) {
-          return res.status(500).send('Cannot load the database');
+        if (!data[request.params.major]) {
+          return response.status(500).send('Cannot load the database');
         }
-        return res.status(200).send(`List: ${data[req.params.major].join(', ')}`);
-      }).catch((err) => {
-        res.status(500).send(`Cannot load the database - ${err.message}`);
+        return response.status(200).send(`List: ${data[request.params.major].join(', ')}`);
+      })
+      .catch(() => {
+        response.status(500).send('Cannot load the database');
       });
   }
 }
--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,25 +1,29 @@
-import fs from 'fs/promises';
+import fs from 'fs';
 
-function readDatabase(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8')
-      .then((data) => {
-        const lines = data.split(/\r?\n/).slice(1);
-        const studentsPerField = {};
-
-        lines.forEach((line) => {
-          const [firstName, , , field] = line.split(',');
-          if (!studentsPerField[field]) {
-            studentsPerField[field] = [firstName];
-          } else {
-            studentsPerField[field].push(firstName);
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) reject(Error('Cannot load the database'));
+    else {
+      const hashtable = {};
+      const lines = data.split('\n');
+      let students = -1;
+      for (const line of lines) {
+        if (line.trim() !== '') {
+          const columns = line.split(',');
+          const field = columns[3];
+          const firstname = columns[0];
+          if (students >= 0) {
+            if (!Object.hasOwnProperty.call(hashtable, field)) {
+              hashtable[field] = [];
+            }
+            hashtable[field] = [...hashtable[field], firstname];
           }
-        });
-        resolve(studentsPerField);
-      }).catch(() => {
-        reject(Error('Cannot load the database'));
-      });
+          students += 1;
+        }
+      }
+      resolve(hashtable);
+    }
   });
-}
+});
 
 export default readDatabase;
