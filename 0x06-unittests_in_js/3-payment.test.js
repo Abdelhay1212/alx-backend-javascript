50e469dc9a46b2a79829985abce1cd49ceb3b936
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-03-16 09:35:29 +0100

    changed expect from chai into assert

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,9 +1,7 @@
 const sinon = require('sinon');
 const Utils = require('./utils.js');
-const chai = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
 
-const expect = chai.expect;
 describe('sendPaymentRequestToApi', () => {
   it('should use Utils.calculateNumber', () => {
     // create a spy on the function Utils.calculateNumber
@@ -11,7 +9,7 @@ describe('sendPaymentRequestToApi', () => {
     // call the function under the test
     sendPaymentRequestToApi(100, 20);
     // Assert that Utils.calculateNumber was called once with the correct arguments
-    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    sinon.assert.calledWith(calculateNumberSpy, 'SUM', 100, 20);
     calculateNumberSpy.restore();
   });
 });
